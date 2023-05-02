5754012a80bf75bb3142a13db45012828e1159ff
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-05-02 15:37:38 +0200

    ALG 02.05

--- JA_0502/src/Main.java
+++ JA_0502/src/Main.java
@@ -3,18 +3,55 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        char[][] c = {{'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}};
-        String s1 = "SEES";
-        String s2 = "ABCB";
-
+//        char[][] c = {{'A', 'B', 'C', 'E'},
+//                {'S', 'F', 'C', 'S'},
+//                {'A', 'D', 'E', 'E'}};
+//        String s1 = "SEES";
+//        String s2 = "ABCB";
 //        System.out.println(exist(c, s2));
-        String inp = "abbaca";
 
-        System.out.println(removeDup(inp));
+//        String inp = "abbaca";
+//        System.out.println(removeDup(inp));
+//
+        String[] s = {"cat", "hat", "bat"};
+        String a = "ctathab";
+        System.out.println(includeChar(s, a));
+        a = "ctathabthab";
+        System.out.println(includeChar(s, a));
     }
 
+    /**
+     * String[] s = {"cat", "hat", "bat"}
+     * String a = "ctathab"
+     */
+
+    public static List<String> includeChar(String[] s, String str) {
+        List<String> result = new ArrayList<>();
+        Map<Character, Integer> mapStr = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+                mapStr.merge(str.charAt(i), 1, Integer::sum);
+        }
+        Map<Character, Integer> temp = new HashMap<>(mapStr);
+        boolean skip = false;
+        for (String word : s) {
+            for (int i = 0; i < word.length(); i++) {
+                if (mapStr.containsKey(word.charAt(i))) {
+                    if (temp.get(word.charAt(i)) <= 0) {
+                        skip = true;
+                        temp = new HashMap<>(mapStr);
+                        continue;
+                    }
+                    temp.merge(word.charAt(i), -1, Integer::sum);
+                }
+            }
+            if (!skip) {
+                result.add(word);
+                skip = false;
+            }
+            mapStr = new HashMap<>(temp);
+        }
+        return result;
+    }
 
     // String inp = "abbaca";
     //abbaca -> a__aca -> aaca -> __ca -> ca
@@ -53,32 +90,4 @@ public class Main {
         return map.values().stream().noneMatch(v -> v < 0);
     }
 
-    /**
-     * String[] s = {"cat", "hat", "bat"}
-     * String a = "ctathab"
-     */
-
-    public static List<String> includeChar(String[] s, String str) {
-        List<String> result = new ArrayList<>();
-        Map<Character, Integer> mapStr = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            mapStr.merge(str.charAt(i), 1, Integer::sum);
-        }
-        Map<Character, Integer> temp = new HashMap<>(mapStr);
-
-        for (String word : s) {
-            for (int i = 0; i < word.length(); i++) {
-                if (mapStr.containsKey(word.charAt(i))) {
-                    if (temp.get(word.charAt(i)) <= 0) {
-                        temp = new HashMap<>(mapStr);
-                        continue;
-                    }
-                    temp.merge(word.charAt(i), -1, Integer::sum);
-                }
-            }
-            result.add(word);
-            mapStr = new HashMap<>(temp);
-        }
-        return result;
-    }
 }
